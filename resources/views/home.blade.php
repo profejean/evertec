5cbca7ffa691f01b749bddc0664fdee9bcd5af03
profejean <profesorjeandiaz@gmail.com>
2022-09-29 12:04:20 -0400

    add change english topics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,17 @@
         <div class="row mt-5 bg-primary p-4 shadow-lg p-3 mb-5 rounded">
             <div class="col-4">
                 <h4 class="text-center text-white">
-                    Nombre del producto
+                    Name of product
                 </h4>
             </div>
             <div class="col-4">
                 <h4 class="text-center text-white">
-                    Precio
+                    Price
                 </h4>
             </div>
             <div class="col-4">
                 <h4 class="text-center text-white">
-                    Opciones
+                    Options
                 </h4>
             </div>
         </div>
@@ -30,7 +30,7 @@
             <div class="col-4 text-center">
               
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#buy-{{$p->id}}">
-                    Ordenar Producto
+                    Order Product
                 </button>
              
                
--- resources/views/modal-buy.blade.php
+++ resources/views/modal-buy.blade.php
@@ -5,17 +5,17 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Comprar producto: {{$p->name}}</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Buy product: {{$p->name}}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <input type="text" placeholder="Nombre" name="name">
-                <input type="email" placeholder="correo" name="email">
-                <input type="text" placeholder="Teléfono" name="tlf">
+                <input type="text" placeholder="Name" name="customer_name">
+                <input type="email" placeholder="Email" name="customer_email">
+                <input type="text" placeholder="Mobile" name="customer_mobile">
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" class="btn btn-primary">Enviar</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Send</button>
             </div>
             </div>
         </div>
