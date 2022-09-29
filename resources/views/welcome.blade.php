a3233ac9c847f53b082485b241ee0653c3e745f2
profejean <profesorjeandiaz@gmail.com>
2022-09-29 12:11:44 -0400

    add change english topics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,22 +5,22 @@
     <div class="container">     
      <div class="login-container">
        <div class="register">
-         <h2>Registrarse</h2>
+         <h2>Register</h2>
         <form class="col text-center" method="post" action="{{route('register')}}">
             @csrf
-            <input type="text" placeholder="Nombre" class="correo" name="name">
-           <input type="email" placeholder="Correo" class="correo" name="email">
-           <input type="password" placeholder="Contraseña" class="pass" name="password">
-           <input type="submit" class="submit" value="REGISTRARSE">
+            <input type="text" placeholder="Name" class="correo" name="name">
+           <input type="email" placeholder="Email" class="correo" name="email">
+           <input type="password" placeholder="Password" class="pass" name="password">
+           <input type="submit" class="submit" value="Register">
          </form>
        </div>
        <div class="login">
-         <h2>Iniciar Sesión</h2>
+         <h2>Login</h2>
           <form class="col text-center" method="post" action="{{route('login')}}">
             @csrf
-           <input type="email" placeholder="Correo" class="correo" name="email">
-           <input type="password" placeholder="Contraseña" class="pass" name="password">
-           <input type="submit" class="submit mt-5 p-2" value="Acceder">
+           <input type="email" placeholder="Email" class="correo" name="email">
+           <input type="password" placeholder="Password" class="pass" name="password">
+           <input type="submit" class="submit mt-5 p-2" value="Login">
          </form>
 
 
