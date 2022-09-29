618922f1a83a81bb25337acf95bffee5e12c1466
profejean <profesorjeandiaz@gmail.com>
2022-09-29 12:06:46 -0400

    add change error

--- resources/views/modal-buy.blade.php
+++ resources/views/modal-buy.blade.php
@@ -9,15 +9,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-<<<<<<< HEAD
-                <input type="text" placeholder="Nombre" name="customer_name">
-                <input type="email" placeholder="correo" name="customer_email">
-                <input type="number" placeholder="Teléfono" name="customer_mobile">
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-=======
+
                 <input type="text" placeholder="Name" name="customer_name">
                 <input type="email" placeholder="Email" name="customer_email">
                 <input type="text" placeholder="Mobile" name="customer_mobile">
@@ -25,7 +17,7 @@
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Send</button>
->>>>>>> 5cbca7ffa691f01b749bddc0664fdee9bcd5af03
+
             </div>
             </div>
         </div>
