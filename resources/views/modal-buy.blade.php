99549deff4d927d2c2e043be613256ff51aa710d
profejean <profesorjeandiaz@gmail.com>
2022-09-29 12:17:06 -0400

    add change button submit

--- resources/views/modal-buy.blade.php
+++ resources/views/modal-buy.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Send</button>
+                <button type="submit" class="btn btn-primary">Send</button>
             </div>
             </div>
         </div>
